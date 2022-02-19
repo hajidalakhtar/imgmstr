a0bbb0b24f3d2018adeca0fdfaf0e74c5c7a3b21
Hajidalakhtar <hajidalakhtar@gmail.com>
2022-02-19 05:50:33 +0000

    popopo

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def image(url:str = None,rotate:str = None,flip:str = None,w:str = None,blur:str
     else:
       return "https://magicimage.hajidalakhtar.repl.co/img?url=https://images.ctfassets.net/23aumh6u8s0i/1T1ohGJuh0B6wqNSwUuXy8/60206973f801267676b9b7f98c34eaea/04_solid_pixels&w=1800&blur=10"
 
-uvicorn.run(app,host="0.0.0.0",port="8080")
+# uvicorn.run(app,host="0.0.0.0",port="8080")
 
 #https://github.com/tiangolo/fastapi
 #https://tryolabs.com/blog/2019/12/10/top-10-python-libraries-of-2019/
